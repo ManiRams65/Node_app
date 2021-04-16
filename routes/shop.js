cc6a1fb0f80cab7988a0b8fc3d61cb5470c14016
V.Manikandan <svvmanikandan@gmail.com>
2021-04-16 20:59:14 +0530

    Add: 1. Template Engine using Handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -2,14 +2,21 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../utils/path");
+const rootDir = require("../util/path");
 const adminData = require("./admin");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  // res.sendFile(path.join(rootDir, "views", "shop.html"));
   const products = adminData.products;
-  res.render("shop", { prods: products, pageTitle: "My Shop", path: "/" });
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 module.exports = router;
